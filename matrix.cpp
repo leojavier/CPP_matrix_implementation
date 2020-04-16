745ea1a5729c84abe4aad2650da5f83b6a40b4fc
Leo Javier <info@leojavier.com>
2020-04-15 21:27:54 -0400

    First commit

---
+++ matrix.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+class Matrix {
+
+  private:
+    std::vector< std::vector<float> > grid;
+    std::vector<int>::size_type rows;
+    std::vector<int>::size_type columns;
+
+};
